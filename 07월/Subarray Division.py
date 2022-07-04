e37bef0e3ee983c12c8baf06023d72529cf3bec2
‘watervin’ <subin04171@gmail.com>
2022-07-05 00:40:50 +0900

    Subarray Division

---
+++ 07월/Subarray Division.py
@@ -0,0 +1,60 @@
+
+from itertools import combinations
+        
+items = ['1', '2', '3', '4', '5']
+        
+        
+          
+          
+
+          
+
+print (list(combinations(items, 2)))
+        
+        
+          
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+from itertools import combinations
+
+def birthday(s, d, m):
+    # Write your code here
+    ans = 0
+    for i in range(len(s)-m+1):
+        if (sum(s[i:i+m]) == d):
+            ans += 1
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
